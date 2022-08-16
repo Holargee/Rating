7e5e5123ce3ae120124beafef3391d1995b9cb2d
Olanibi Gideon Adeshina <101959350+Holargee@users.noreply.github.com>
2022-08-16 21:09:52 +0100

    Create rating.js

---
+++ rating.js
@@ -0,0 +1,20 @@
+function btn() 
+{
+document.getElementById("b2").style.display="none";
+document.getElementById("b3").style.display="block";
+}
+function bu1(){
+    document.getElementById("no").innerHTML = "1";
+}
+function bu2(){
+    document.getElementById("no").innerHTML = "2";
+}
+function bu3(){
+    document.getElementById("no").innerHTML = "3";
+}
+function bu4(){
+    document.getElementById("no").innerHTML = "4";
+}
+function bu5(){
+    document.getElementById("no").innerHTML = "5";
+}
